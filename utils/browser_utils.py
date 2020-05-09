120b7d186b2980d440b78cc4ebd32cc0480fbf7b
John Ward <jsward2@gmail.com>
2020-05-09 18:37:18 -0400

    refactor: add utils package

--- utils/browser_utils.py
+++ utils/browser_utils.py
@@ -1,3 +1,7 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+
+
 def find_by_text(browser, text):
     return browser.find_elements_by_xpath("//*[text()='{}']".format(text))
 
@@ -9,10 +13,15 @@ def get_browser(headless=False):
     return Chrome("/Users/john/csci-e-29/grocer/chromedriver", options=opts)
 
 
-def get_child_text(element):
+def get_children(element):
     for elem in element.find_elements_by_xpath(".//*"):
-        yield elem.text
+        yield elem
 
 
 def get_parent(element):
     return element.find_elements_by_xpath("..")[0]
+
+
+def get_siblings(element):
+    for elem in element.find_elements_by_xpath("../*"):
+        yield elem
