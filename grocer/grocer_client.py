06dc8367ead410f0f756a701dee88abeed92ee8d
John Ward <jsward2@gmail.com>
2020-05-11 23:43:35 -0400

    feat: update client to take merchant and email and password as inputs

--- grocer/grocer_client.py
+++ grocer/grocer_client.py
@@ -6,11 +6,11 @@ from .wegmans.tasks import GetDeliveryTimes
 
 
 class GrocerClient:
-    def __init__(self):
-        # TODO: email and password and merchant as inputs and remove hard-coding
-        self.merchant = "wegmans"
-        self.email = os.environ["EMAIL"]
-        self.password = os.environ["PASSWORD"]
+    def __init__(self, merchant, email, password):
+        # TODO: use email and password and merchant as params to task
+        self.merchant = merchant
+        self.email = email
+        self.password = password
 
     def get_delivery_times(self):
         delivery_times = GetDeliveryTimes()
