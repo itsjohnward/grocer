f0fb70e82cdcc01b22720062f93bed8b36cb0a14
John Ward <jsward2@gmail.com>
2020-05-11 23:29:21 -0400

    fix: update cli to import from wegmans

--- grocer/cli.py
+++ grocer/cli.py
@@ -2,11 +2,12 @@ import argparse
 
 import luigi
 
-from .instacart.tasks import GetDeliveryTimes
+from .wegmans.tasks import GetDeliveryTimes
 
 
 def main():
     parser = argparse.ArgumentParser(description="Grocer CLI")
+    # TODO: take merchant and username and password as inputs and pass to task
     args = parser.parse_args()
     delivery_times = GetDeliveryTimes()
     luigi.build([delivery_times], local_scheduler=True)
