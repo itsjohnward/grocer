353ac5128172840fd5c4b5d928cc7f6f689b0338
John Ward <jsward2@gmail.com>
2020-05-11 23:37:06 -0400

    feat: update cli to match README

--- grocer/cli.py
+++ grocer/cli.py
@@ -7,8 +7,14 @@ from .wegmans.tasks import GetDeliveryTimes
 
 def main():
     parser = argparse.ArgumentParser(description="Grocer CLI")
+    parser.add_argument("merchant", action="store", type=str, choices=["wegmans"])
+    parser.add_argument("action", action="store", type=str, choices=["times"])
     # TODO: take merchant and username and password as inputs and pass to task
+    parser.add_argument("-e", "--email", action="store", type=str)
+    parser.add_argument("-p", "--password", action="store", type=str)
     args = parser.parse_args()
-    delivery_times = GetDeliveryTimes()
-    luigi.build([delivery_times], local_scheduler=True)
-    delivery_times.print_results()
+
+    if args.action == "times":
+        delivery_times = GetDeliveryTimes()
+        luigi.build([delivery_times], local_scheduler=True)
+        delivery_times.print_results()
